9f7eaef3af66e9c8868df36fabc04ba4d2f36242
Blindcoke <y.lashkul.io16@kpi.ua>
2024-02-26 17:32:14 +0900

    tensorrt roberta

--- ai/main.py
+++ ai/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from transformers import pipeline, RobertaTokenizer
+from transformers import pipeline, RobertaTokenizer, PretrainedConfig
 from optimum.onnxruntime import ORTModelForSequenceClassification
+from onnxruntime import InferenceSession
 import torch
 import re
 import logging
 import io
-from typing import Dict
+import json
+from typing import Dict 
 from faster_whisper import WhisperModel
 logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
@@ -22,9 +24,20 @@ app.add_middleware(
 model = WhisperModel("./models/w-base/", device="cuda")
 # classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None, device=torch.cuda.set_device(0))
 classifier_directory = "./models/onnx/roberta/"
-roberta = ORTModelForSequenceClassification.from_pretrained(classifier_directory, file_name="model.onnx")
+# roberta = ORTModelForSequenceClassification.from_pretrained(classifier_directory, file_name="model.onnx")
+config = None
+with open("./models/onnx/roberta/config.json", "r") as f:
+    config = json.load(f)
+# roberta = ORTModelForSequenceClassification(config=PretrainedConfig(**config), model=InferenceSession('./models/onnx/roberta/model.onnx', providers=['CUDAExecutionProvider']))
+trt_ep_options = {
+    "trt_engine_cache_enable": True,
+    "trt_profile_min_shapes": "input_ids:1x1,attention_mask:1x1",
+    "trt_profile_max_shapes": "input_ids:1x512,attention_mask:1x512",
+    "trt_profile_opt_shapes": "input_ids:1x512,attention_mask:1x512",
+}
+roberta = ORTModelForSequenceClassification(config=PretrainedConfig(**config), model=InferenceSession('./models/onnx/roberta/model.onnx', providers=[("TensorrtExecutionProvider", trt_ep_options)]))
 classifier = pipeline(
-    "sentiment-analysis",
+    "text-classification",
     model=roberta,
     tokenizer = RobertaTokenizer.from_pretrained("./models/roberta-base/"),
     top_k=None,
@@ -43,27 +56,27 @@ special_symbols = '(?:\.+ |! |\? |; )'
 
 @app.post("/predict")
 async def predict(data: InputSentence):
-    try:
-        predictions = []
-        print(data.text)
-        data.text += " "
-        symbol_emojies = re.findall(special_symbols, data.text)
-        print(symbol_emojies)
-        sentences = re.split(special_symbols, data.text)
-        if not sentences[-1]:
-            sentences.pop()
-        for s in sentences:
-            predictions += classifier(s)
-        logging.info("Predictions: %s", predictions)
-        response_data = {
-            "predictions": predictions,
-            "sentences" : sentences,
-            "symbol_emojies" : symbol_emojies
-        }
-        return JSONResponse(content=response_data , status_code=200)
-    except Exception as e:
-        logging.error("Error: %s", e)
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+    # try:
+    predictions = []
+    print(data.text)
+    data.text += " "
+    symbol_emojies = re.findall(special_symbols, data.text)
+    print(symbol_emojies)
+    sentences = re.split(special_symbols, data.text)
+    if not sentences[-1]:
+        sentences.pop()
+    for s in sentences:
+        predictions += classifier(s)
+    logging.info("Predictions: %s", predictions)
+    response_data = {
+        "predictions": predictions,
+        "sentences" : sentences,
+        "symbol_emojies" : symbol_emojies
+    }
+    return JSONResponse(content=response_data , status_code=200)
+    # except Exception as e:
+    #     logging.error("Error: %s", e)
+    #     return JSONResponse(content={"error": str(e)}, status_code=500)
 
 def transcribe(audio: bytes) -> str:
     segments, _ = model.transcribe(io.BytesIO(audio))
@@ -71,29 +84,6 @@ def transcribe(audio: bytes) -> str:
     print(recognized_text)
     return recognized_text
 
-@app.post("/predict")
-async def predict(data: InputSentence):
-    try:
-        predictions = []
-        print(data.text)
-        data.text += " "
-        symbol_emojies = re.findall(special_symbols, data.text)
-        print(symbol_emojies)
-        sentences = re.split(special_symbols, data.text)
-        if not sentences[-1]:
-            sentences.pop()
-        for s in sentences:
-            predictions += classifier(s)
-        logging.info("Predictions: %s", predictions)
-        response_data = {
-            "predictions": predictions,
-            "sentences" : sentences,
-            "symbol_emojies" : symbol_emojies
-        }
-        return JSONResponse(content=response_data , status_code=200)
-    except Exception as e:
-        logging.error("Error: %s", e)
-        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 
